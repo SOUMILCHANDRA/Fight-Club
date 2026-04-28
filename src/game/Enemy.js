95202aceb27579ba519f174c40e110318390d5d5
SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
2026-04-28 21:26:56 +0530

    docs(E:\Fight Club\src\game\Enemy.js): add file-level documentation and author attribution

--- src/game/Enemy.js
+++ src/game/Enemy.js
@@ -1,5 +1,11 @@
 /**
  * @file E:\Fight Club\src\game\Enemy.js
+ * @author SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
+ * @description Senior implementation for Project Mayhem.
+ * @version 1.0.0
+ */
+/**
+ * @file E:\Fight Club\src\game\Enemy.js
  * @description Senior implementation for Project Mayhem.
  */
 import { Entity } from './Entity.js';
