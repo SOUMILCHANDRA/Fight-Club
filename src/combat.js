9e6a26006b38c73c5927b66470ef283531b28cf9
SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
2026-04-28 21:26:49 +0530

    docs(E:\Fight Club\src\combat.js): add file-level documentation and author attribution

--- src/combat.js
+++ src/combat.js
@@ -1,5 +1,11 @@
 /**
  * @file E:\Fight Club\src\combat.js
+ * @author SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
+ * @description Senior implementation for Project Mayhem.
+ * @version 1.0.0
+ */
+/**
+ * @file E:\Fight Club\src\combat.js
  * @description Senior implementation for Project Mayhem.
  */
 /**
