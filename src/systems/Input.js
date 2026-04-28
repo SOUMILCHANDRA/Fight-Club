87f5bc7341bbb937eb8d69f9dba5e16fc619378f
SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
2026-04-28 21:30:33 +0530

    docs(E:\Fight Club\src\systems\Input.js): add file-level documentation and author attribution

--- src/systems/Input.js
+++ src/systems/Input.js
@@ -6,6 +6,12 @@
  */
 /**
  * @file E:\Fight Club\src\systems\Input.js
+ * @author SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
+ * @description Senior implementation for Project Mayhem.
+ * @version 1.0.0
+ */
+/**
+ * @file E:\Fight Club\src\systems\Input.js
  * @description Senior implementation for Project Mayhem.
  */
 export class Input {
