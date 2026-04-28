c50795edeb082b9528b50a29c8c91c7299cf0beb
SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
2026-04-28 21:26:55 +0530

    feat: implement node_modules/three/examples/jsm/nodes/core/UniformNode.js

--- src/game/ClubManager.js
+++ src/game/ClubManager.js
@@ -1,5 +1,11 @@
 /**
  * @file E:\Fight Club\src\game\ClubManager.js
+ * @author SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
+ * @description Senior implementation for Project Mayhem.
+ * @version 1.0.0
+ */
+/**
+ * @file E:\Fight Club\src\game\ClubManager.js
  * @description Senior implementation for Project Mayhem.
  */
 export class ClubManager {
