5108d7ad5a9afe3ab759a29f9cf6cb3e41286a50
SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
2026-04-28 21:26:52 +0530

    docs(E:\Fight Club\src\personalitySystem.js): add file-level documentation and author attribution

--- src/personalitySystem.js
+++ src/personalitySystem.js
@@ -1,5 +1,11 @@
 /**
  * @file E:\Fight Club\src\personalitySystem.js
+ * @author SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
+ * @description Senior implementation for Project Mayhem.
+ * @version 1.0.0
+ */
+/**
+ * @file E:\Fight Club\src\personalitySystem.js
  * @description Senior implementation for Project Mayhem.
  */
 /**
