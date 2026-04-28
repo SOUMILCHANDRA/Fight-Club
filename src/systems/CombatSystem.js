e4cfc9029ae0e4d1439db5ccbc451bd436de9c72
SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
2026-04-28 21:26:58 +0530

    feat: implement node_modules/three/examples/jsm/nodes/display/ColorSpaceNode.js

--- src/systems/CombatSystem.js
+++ src/systems/CombatSystem.js
@@ -1,5 +1,11 @@
 /**
  * @file E:\Fight Club\src\systems\CombatSystem.js
+ * @author SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
+ * @description Senior implementation for Project Mayhem.
+ * @version 1.0.0
+ */
+/**
+ * @file E:\Fight Club\src\systems\CombatSystem.js
  * @description Senior implementation for Project Mayhem.
  */
 import * as THREE from 'three';
