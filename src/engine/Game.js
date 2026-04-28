bd62476a382eed568be13233085ad305122a4666
SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
2026-04-28 21:30:29 +0530

    docs(E:\Fight Club\src\engine\Game.js): add file-level documentation and author attribution

--- src/engine/Game.js
+++ src/engine/Game.js
@@ -6,6 +6,12 @@
  */
 /**
  * @file E:\Fight Club\src\engine\Game.js
+ * @author SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
+ * @description Senior implementation for Project Mayhem.
+ * @version 1.0.0
+ */
+/**
+ * @file E:\Fight Club\src\engine\Game.js
  * @description Senior implementation for Project Mayhem.
  */
 import * as THREE from 'three';
