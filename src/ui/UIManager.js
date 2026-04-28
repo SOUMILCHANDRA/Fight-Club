3c7d3e62195f4c44171c3ad074c19503c0044958
SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
2026-04-28 21:26:59 +0530

    feat: implement node_modules/three/examples/jsm/nodes/display/PosterizeNode.js

--- src/ui/UIManager.js
+++ src/ui/UIManager.js
@@ -1,5 +1,11 @@
 /**
  * @file E:\Fight Club\src\ui\UIManager.js
+ * @author SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
+ * @description Senior implementation for Project Mayhem.
+ * @version 1.0.0
+ */
+/**
+ * @file E:\Fight Club\src\ui\UIManager.js
  * @description Senior implementation for Project Mayhem.
  */
 export class UIManager {
