01ae080f5f3651c1be68398e0d825d1c919e9f50
SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
2026-04-28 21:30:31 +0530

    docs(E:\Fight Club\src\game\ClubManager.js): add file-level documentation and author attribution

--- src/game/ClubManager.js
+++ src/game/ClubManager.js
@@ -6,6 +6,12 @@
  */
 /**
  * @file E:\Fight Club\src\game\ClubManager.js
+ * @author SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
+ * @description Senior implementation for Project Mayhem.
+ * @version 1.0.0
+ */
+/**
+ * @file E:\Fight Club\src\game\ClubManager.js
  * @description Senior implementation for Project Mayhem.
  */
 export class ClubManager {
